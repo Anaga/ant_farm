a7925dee82c3895af04b37b12c41c87f1d1f58f5
bby <Aleksandr.Bobyrev@stoneridge.com>
2023-12-12 23:15:19 +0200

    finished refactoring the configuration parsing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
-use std::{env, process::exit};
+use std::env;
 use std::fs;
+use std::error::Error;
+use std::process;
 
 fn main() {
-    println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("We need more args!");        
-        exit(0);
-    }
-    
-    let file_path = &args[1];
+   
+    let file_path = parse_config(&args);
     
     println!("In file {}", file_path);
-    let contents = fs::read_to_string(file_path)
-    .expect("Should have been able to read the file");
+    
+    if let Err(e) = run(file_path) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }    
+}
+
+fn run(file_path: &str) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(file_path)?;
 
     println!("With text:\n{contents}");
+    Ok(())
+}
+
+fn parse_config(args: &[String]) -> &str {
+    if args.len() < 2 {
+        println!("We need more args!"); 
+        panic!("not enough arguments");            
+    }
+    let file_path = &args[1];
+    file_path
 }
